b66c23b0cef51a231a0205cff2e209ebe8f9dada
Sophie <peletsophie@gmail.com>
2026-06-12 11:01:32 +0200

    refactor: add explanatory comments about the use of references and pointers in ex03

--- ex03/src/HumanA.cpp
+++ ex03/src/HumanA.cpp
@@ -6,13 +6,17 @@
 /*   By: sopelet <sopelet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/11 16:12:55 by sopelet           #+#    #+#             */
-/*   Updated: 2026/06/11 17:35:13 by sopelet          ###   ########.fr       */
+/*   Updated: 2026/06/12 10:57:25 by sopelet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Weapon.hpp"
 #include "HumanA.hpp"
 
+/*
+	HumanA always have a weapon and this weapon won't change during his lifetime
+	That's why `_weapon` is a reference (can't be NULL and is immutable)
+*/
 HumanA::HumanA(std::string name, Weapon& weapon) : _weapon(weapon), _name(name) {
 }
 
--- ex03/src/HumanB.cpp
+++ ex03/src/HumanB.cpp
@@ -6,7 +6,7 @@
 /*   By: sopelet <sopelet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/11 16:13:10 by sopelet           #+#    #+#             */
-/*   Updated: 2026/06/11 17:41:20 by sopelet          ###   ########.fr       */
+/*   Updated: 2026/06/12 10:56:59 by sopelet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,13 @@ HumanB::HumanB(std::string name) {
 HumanB::~HumanB() {
 }
 
+/*
+	HumanB has a pointer to `_weapon` as he may not always have a weapon
+	Pointers can be set to NULL (not references) and they can be changed through
+	the lifetime of the human
+	This function takes a weapon as a reference as the `main` passes it as 
+	an object and not as a pointer
+*/
 void	HumanB::setWeapon(Weapon& weapon) {
 	_weapon = &weapon;
 }
