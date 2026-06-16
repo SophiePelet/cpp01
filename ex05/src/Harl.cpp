90da209a974d72110ae5fc15a687249341206374
Sophie <peletsophie@gmail.com>
2026-06-16 12:24:12 +0200

    refactor: fix typo in the Harl messages

--- ex05/src/Harl.cpp
+++ ex05/src/Harl.cpp
@@ -6,7 +6,7 @@
 /*   By: sopelet <sopelet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/12 17:21:55 by sopelet           #+#    #+#             */
-/*   Updated: 2026/06/16 12:04:33 by sopelet          ###   ########.fr       */
+/*   Updated: 2026/06/16 12:22:17 by sopelet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,12 +24,12 @@ void	Harl::debug() {
 }
 
 void	Harl::info() {
-	std::cout << "I cannot believe adding extra bacon costs more money. You didn't put"
+	std::cout << "I cannot believe adding extra bacon costs more money. You didn't put "
 "enough bacon in my burger! If you did, I wouldn't be asking for more!\n";
 }
 
 void	Harl::warning() {
-	std::cout << "I think I deserve to have some extra bacon for free. I've been coming for"
+	std::cout << "I think I deserve to have some extra bacon for free. I've been coming for "
 "years, whereas you started working here just last month.\n";
 }
 
