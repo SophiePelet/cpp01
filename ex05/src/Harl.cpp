530e8ca2942273c19f23d1ea6e9a63fa4c1dc9a7
Sophie <peletsophie@gmail.com>
2026-06-16 12:04:48 +0200

    refactor: add explanatory comment to the `complain` function

--- ex05/src/Harl.cpp
+++ ex05/src/Harl.cpp
@@ -6,7 +6,7 @@
 /*   By: sopelet <sopelet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/12 17:21:55 by sopelet           #+#    #+#             */
-/*   Updated: 2026/06/16 11:24:39 by sopelet          ###   ########.fr       */
+/*   Updated: 2026/06/16 12:04:33 by sopelet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,6 +37,12 @@ void	Harl::error() {
 	std::cout << "This is unacceptable! I want to speak to the manager now.\n";
 }
 
+/*
+	This function uses an array of pointers to member functions and a look up table
+	This prevents us from using a bunch of if/else statements
+	It iterates through an array of strings and compare its content with `level`
+	If there is a match, it calls the corresponding pointer to member function
+*/
 void	Harl::complain(std::string level) {
 	std::string	whine[] = {"DEBUG", "INFO", "WARNING", "ERROR"};
 	void	(Harl::*ptr_complains[])(void) = {
