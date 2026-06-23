c0f060f86a85d60d6815bef799ffed4d72a123c4
Sophie <peletsophie@gmail.com>
2026-06-23 11:27:54 +0200

    feat: print new info in the `main` to improve clarity

--- ex05/main.cpp
+++ ex05/main.cpp
@@ -6,7 +6,7 @@
 /*   By: sopelet <sopelet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/12 17:29:18 by sopelet           #+#    #+#             */
-/*   Updated: 2026/06/16 17:34:10 by sopelet          ###   ########.fr       */
+/*   Updated: 2026/06/23 11:27:32 by sopelet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,15 @@
 int	main(void) {
 	Harl	harl;
 
+	std::cout << "[ DEBUG ]\n";
 	harl.complain("DEBUG");
+	std::cout << "[ Info ]\n";
 	harl.complain("INFO");
+	std::cout << "[ WARNING ]\n";
 	harl.complain("WARNING");
+	std::cout << "[ ERROR ]\n";
 	harl.complain("ERROR");
+	std::cout << "[ UNKNOWN ]\n";
 	harl.complain("UNKNOWN");
 	
 	return (0);
